47d74883ae5884022cac3d8ac9a7d73ece959131
Isa <IsabellaGreco@omrits-air.home>
2024-11-20 11:13:45 +0000

    code

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -39,7 +39,7 @@ export default function LandingPage({ onGetStarted }: LandingPageProps) {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1540555700478-4be289fbecef?auto=format&fit=crop&q=80&w=2000&h=1000"
+            src="https://cdn.midjourney.com/6d802b2b-0a87-4bf4-8cc1-feb757b9485d/0_0.png"
             alt="Luxury retirement lifestyle"
           />
         </div>
